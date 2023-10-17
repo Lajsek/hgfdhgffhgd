fef023cffac5c11fedc4ef0a9d80127968bc9a2d
student <student@pollub.edu.pl>
2023-10-17 11:47:10 +0200

    Initial Commit

---
+++ hgfdhgffhgd/hgfdhgffhgdApp.swift
@@ -0,0 +1,17 @@
+//
+//  hgfdhgffhgdApp.swift
+//  hgfdhgffhgd
+//
+//  Created by student on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct hgfdhgffhgdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
